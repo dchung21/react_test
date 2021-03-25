db1af6e824fc7c6f6d1752b63d3045230e0de9e6
Cindy T <cindyt388@gmail.com>
2021-03-25 15:19:35 -0700

    Update OpenHours and CloseHours

--- client/src/admin/EditForm.js
+++ client/src/admin/EditForm.js
@@ -1,6 +1,7 @@
 import React, { useEffect, useState }  from 'react';
 import ClinicForm from './ClinicForm.js';
 import Axios from 'axios';
+import DateFnsUtils from '@date-io/date-fns';
 
 export default function EditForm(props) {
 
@@ -24,8 +25,18 @@ export default function EditForm(props) {
 	let fillOpenHours = [];
 	let fillCloseHours = [];
 	for (let i = 0; i < 7; i++) {
-		fillOpenHours[i] = data.rows[i].hour_open;
-		fillCloseHours[i] = data.rows[i].hour_close;
+		let o = data.rows[i].hour_open.split(/[:]/);
+		const openHoursObj = new Date('2014-08-18T00:00:00');
+		openHoursObj.setHours(o[0]);
+		openHoursObj.setMinutes(o[1]);
+		//dateObject.setSeconds(s[2]);
+		fillOpenHours[i] = openHoursObj;
+
+		let c = data.rows[i].hour_close.split(/[:]/);
+		const closeHoursObj = new Date('2014-08-18T00:00:00');
+		closeHoursObj.setHours(c[0]);
+		closeHoursObj.setMinutes(c[1]);
+		fillCloseHours[i] = closeHoursObj;
 	}
 	
 	let info = {
